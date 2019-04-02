e1c34b0e3157c333151a78021c5174462cef4837
likejackonfire <gravitybringsmedown@gmail.com>
2019-04-02 08:56:08 -0700

    1 pass and 1 fail on sort drill

--- test/sort.test.js
+++ test/sort.test.js
@@ -1,3 +1,16 @@
 const expect = require('chai').expect;
 
 const sort = require('../index');
+
+describe('Sort function', () => {
+    it('should sort the array in ascending order', () => {
+        expect(sort([3, 4, 2, 5, 1])).to.deep.equal([1,2, 3, 4, 5]);
+    });
+
+    it ('should throw an error if the input is not an array', () => {
+        expect(() => {sort('string')}).to.throw();
+        
+        // const fn = () => { sort('string') };
+        // expect(fn).to.throw();
+    });
+});
